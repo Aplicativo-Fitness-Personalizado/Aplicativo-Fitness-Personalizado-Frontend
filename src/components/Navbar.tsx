120ae2eaac6f668a394707f1e07b93282e09cfba
Bowie <larissa.soaress@yahoo.com.br>
2025-06-04 15:12:18 -0300

    Implementação do Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { SignOut, User } from "@phosphor-icons/react";
+import Logo from "../assets/img/logo.png"
+import { Link, useNavigate } from 'react-router';
+
+
+function Navbar() {
+
+    const navigate = useNavigate();
+
+    function logout() {
+
+        navigate('/')
+    }
+
+    return (
+        <header className="w-full bg-background text-light shadow-md flex items-center justify-between px-6 py-3">
+
+            <Link to="/" className="flex items-center gap-4 border-border">
+                <img src={Logo} alt="Logo FitLab" className="h-12" />
+            </Link>
+
+            <div className="flex items-center gap-6">
+                <Link to="/" className="flex items-center gap-6 border-border bg-black text-light text-lg px-6 py-3 rounded ">
+                    <span>Perfil</span>
+                    <User size={24} weight="bold" />
+                </Link>
+
+                <Link to="/" onClick={logout} className="flex items-center gap-8 border-border bg-red-500 text-light text-lg px-6 py-3 rounded ">
+                    <span>Sair</span>
+                    <SignOut size={20} weight="bold" />
+                </Link>
+            </div>
+        </header>
+
+    )
+}
+
+export default Navbar
