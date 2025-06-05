7864757e699561cf045d0a954a0b0ff4ffae9b2e
wdwf <w_dwf@hotmail.com>
2025-06-05 10:20:47 -0300

    :lipstick: refactor: update Navbar component to use correct icon imports and fix link paths

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { SignOut, User } from "@phosphor-icons/react";
+import { SignOutIcon, UserIcon } from "@phosphor-icons/react";
 import Logo from "../assets/img/logo.png"
 import { Link, useNavigate } from 'react-router';
 
@@ -15,19 +15,19 @@ function Navbar() {
     return (
         <header className="w-full bg-background text-light shadow-md flex items-center justify-between px-6 py-3">
 
-            <Link to="/" className="flex items-center gap-4 border-border">
+            <Link to="/home" className="flex items-center gap-4 border-border">
                 <img src={Logo} alt="Logo FitLab" className="h-12" />
             </Link>
 
             <div className="flex items-center gap-6">
-                <Link to="/" className="flex items-center gap-6 border-border bg-black text-light text-lg px-6 py-3 rounded ">
+                <Link to="/perfil" className="flex items-center gap-6 border-border bg-black text-light text-lg px-6 py-3 rounded ">
                     <span>Perfil</span>
-                    <User size={24} weight="bold" />
+                    <UserIcon size={24} weight="bold" />
                 </Link>
 
-                <Link to="/" onClick={logout} className="flex items-center gap-8 border-border bg-red-500 text-light text-lg px-6 py-3 rounded ">
+                <Link to="/login" onClick={logout} className="flex items-center gap-8 border-border bg-red-500 text-light text-lg px-6 py-3 rounded ">
                     <span>Sair</span>
-                    <SignOut size={20} weight="bold" />
+                    <SignOutIcon size={20} weight="bold" />
                 </Link>
             </div>
         </header>
