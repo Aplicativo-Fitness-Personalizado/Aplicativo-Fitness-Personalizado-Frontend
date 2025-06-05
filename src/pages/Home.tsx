50ac97934975087fe4e8ccadc2ba14ab12a94d8d
Rodrigo Henrique <rhs.buiu@gmail.com>
2025-06-05 01:48:39 -0300

    Aprimoramento da página home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -64,30 +64,30 @@ function Home() {
         
               <div className="grid md:grid-cols-3 gap-4 mb-8 p-3">
 
-                <div className="bg-white rounded-lg shadow-lg shadow-gray-600 w-[392px] h-[336px] px-6 py-4 border border-gray-200">
-                  <div className="grid grid-cols-2 text-gray-500 text-sm mb-2">
-                    <div>
+                <div className="bg-white rounded-xl cursor-pointer shadow-md hover:shadow-xl shadow-gray-600 transition-shadow duration-300 w-[392px] h-[336px] px-6 py-4 border-1 border-gray-200">
+                  <div className="grid grid-cols-2 text-gray-500 mb-2">
+                    <div className="">
                       <p className="font-normal">Título</p>
                       <p className="text-black font-semibold">Remada baixa</p>
                     </div>
-                    <div>
+                    <div className="ml-7">
                       <p className="font-normal">Região Trabalhada</p>
                       <p className="text-black font-semibold">Costas</p>
                     </div>
                   </div>
 
-                  <div className="grid grid-cols-2 text-gray-500 text-sm mb-2">
+                  <div className="grid grid-cols-2 text-gray-500  mb-2">
                     <div>
                       <p className="font-normal">Tempo de Descanso</p>
                       <p className="text-black font-semibold">60min</p>
                     </div>
-                    <div>
+                    <div className="ml-7">
                       <p className="font-normal">Repetição</p>
                       <p className="text-black font-semibold">4×10</p>
                     </div>
                   </div>
 
-                  <div className="text-gray-500 text-sm mb-4 pb-20">
+                  <div className="text-gray-500 mb-4 pb-13">
                     <p className="font-normal">Descrição</p>
                     <p className="text-black font-semibold">Trabalha os musculos musculares</p>
                   </div>
@@ -104,6 +104,49 @@ function Home() {
                   </button>
                 </div>
             </div>
+
+            <div className="bg-white rounded-xl cursor-pointer shadow-md hover:shadow-xl shadow-gray-600 transition-shadow duration-300 w-[392px] h-[336px] px-6 py-4 border-1 border-gray-200 ">
+                  <div className="grid grid-cols-2 text-gray-500 mb-2">
+                    <div>
+                      <p className="font-normal">Título</p>
+                      <p className="text-black font-semibold">Remada baixa</p>
+                    </div>
+                    <div className="ml-7">
+                      <p className="font-normal">Região Trabalhada</p>
+                      <p className="text-black font-semibold">Costas</p>
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-2 text-gray-500 mb-2">
+                    <div>
+                      <p className="font-normal">Tempo de Descanso</p>
+                      <p className="text-black font-semibold">60min</p>
+                    </div>
+                    <div className="ml-7">
+                      <p className="font-normal">Repetição</p>
+                      <p className="text-black font-semibold">4×10</p>
+                    </div>
+                  </div>
+
+                  <div className="text-gray-500 pb-13">
+                    <p className="font-normal">Descrição</p>
+                    <p className="text-black font-semibold">Trabalha os musculos musculares</p>
+                  </div>
+
+               <div className="flex justify-between mt-4">
+                  <button className="cursor-pointer font-semibold px-4 py-1 rounded-lg bg-gray-700 text-white hover:bg-gray-900 transition-colors duration-1000 w-[150px] h-[50px]">
+                    <img src={Editar} alt="Logo do lápis" className="w-[14px] h-[16px] inline-block mr-2" />
+                    Editar
+
+                  </button>
+                  <button className="cursor-pointer font-semibold px-4 py-1 rounded-lg bg-[#ff2d55] text-white hover:bg-red-600 transition-colors duration-1000 w-[150px] h-[50px]">
+                    <img src={Excluir} alt="Logo da Lixeixa" className="w-[14px] h-[16px] inline-block mr-2" />
+                    Excluir
+                  </button>
+                </div>
+            </div>
+
+            
             
           </div>
         </div>
